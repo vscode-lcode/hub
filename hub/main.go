a03207513d82daa865d0bd444dd2b6acebec8575
shynome <shynome@gmail.com>
2022-07-08 23:52:15 +0800

    add cli options Foreground

--- hub/main.go
+++ hub/main.go
@@ -9,15 +9,18 @@ import (
 )
 
 var args struct {
-	Addr string
+	Addr       string
+	Foreground bool
 }
 
 func init() {
 	flag.StringVar(&args.Addr, "l", "127.0.0.1:4349", "Bind address")
-	flag.Parse()
+	flag.BoolVar(&args.Foreground, "f", false, "Run Hub foreground")
 }
 
 func main() {
+	flag.Parse()
+
 	stopChan := make(chan struct{})
 	mux := http.DefaultServeMux
 
@@ -32,6 +35,10 @@ func main() {
 		os.Exit(0)
 	}
 	http.HandleFunc("/exit", func(w http.ResponseWriter, r *http.Request) {
+		if args.Foreground {
+			fmt.Fprint(w, "exit signal was ignored")
+			return
+		}
 		go exitProgram()
 		fmt.Fprint(w, "exit signal has received")
 	})
