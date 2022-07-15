81f1d6197c2d8a5bc3ab25bc7c9d8e9fbe5ec6bc
shynome <shynome@gmail.com>
2022-07-15 12:50:51 +0800

    lcode-hub init move to ssh config local command

--- extension.js
+++ extension.js
@@ -7,17 +7,18 @@ const { Opener } = require("./opener");
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
-  const hub = new Hub();
-  context.subscriptions.push(hub);
+  // const hub = new Hub();
+  // context.subscriptions.push(hub);
 
-  const hubInit = hub.init();
+  // const hubInit = hub.init();
+  const hubInit = Promise.resolve(0);
   const lcodeUriHandler = new UriHandler(hubInit);
   context.subscriptions.push(vscode.window.registerUriHandler(lcodeUriHandler));
 
-  lcodeUriHandler.preTask.then(() => {
-    const opener = new Opener(lcodeUriHandler);
-    context.subscriptions.push(opener);
-  });
+  // lcodeUriHandler.preTask.then(() => {
+  //   const opener = new Opener(lcodeUriHandler);
+  //   context.subscriptions.push(opener);
+  // });
 }
 
 // this method is called when your extension is deactivated
