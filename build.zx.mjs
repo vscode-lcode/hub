b5dca76390690a6cc50815111e853af104a46d41
shynome <shynome@gmail.com>
2023-01-15 23:29:39 +0800

    使用 zx 写脚本

---
+++ build.zx.mjs
@@ -0,0 +1,37 @@
+#!/usr/bin/env zx
+
+/**@typedef {import("zx/globals")} */
+
+const targets = [
+  ["windows", "amd64", "win32-x64"],
+  ["windows", "386", "win32-ia32"],
+  ["windows", "arm64", "win32-arm64"],
+  ["linux", "amd64", "linux-x64"],
+  ["linux", "arm64", "linux-arm64"],
+  // ["linux", "armhf", "linux-armhf"],
+  ["linux", "amd64", "alpine-x64"],
+  ["linux", "arm64", "alpine-arm64"],
+  ["darwin", "amd64", "darwin-x64"],
+  ["darwin", "arm64", "darwin-arm64"],
+];
+
+const baseUrl =
+  "https://github.com/vscode-lcode/lcode/releases/latest/download/";
+
+const downloadFailed = Symbol("download failed");
+
+await $`rm -rf *.vsix`;
+
+for (let index = 0; index < targets.length; index++) {
+  let t = targets[index];
+  console.log(`[${index}/${targets.length}] progress`);
+  let binLink = new URL(`lcode-hub-${t[0]}-${t[1]}`, baseUrl);
+  let download = $`wget -qO bin/lcode-hub ${binLink}`;
+  let result = await download.catch(() => downloadFailed);
+  if (result === downloadFailed) {
+    console.error(`skip target ${t}`);
+    continue;
+  }
+  await $`yarn vsce package -t ${t[2]}`;
+  // $`wget -O bin/lcode-hub ${t}`;
+}
