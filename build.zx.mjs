20446d4954567fd0f344afb7a0a306d305551034
shynome <shynome@gmail.com>
2023-07-06 10:53:29 +0800

    upgrade lcode v0.0.6

--- build.zx.mjs
+++ build.zx.mjs
@@ -17,7 +17,7 @@ const targets = [
 
 const baseUrl =
   // "https://github.com/vscode-lcode/lcode/releases/latest/download/";
-  "https://github.com/vscode-lcode/lcode-hub/releases/download/v0.0.5/";
+  "https://github.com/vscode-lcode/lcode-hub/releases/download/v0.0.6/";
 
 const downloadFailed = Symbol("download failed");
 
