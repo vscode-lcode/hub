512609e1596d15e2c5aba572e179d83fce8aca69
shynome <shynome@gmail.com>
2024-07-23 23:47:47 +0800

    添加 main.Version 变量

--- cmd/root.go
+++ cmd/root.go
@@ -35,7 +35,8 @@ var rootCmd = &cobra.Command{
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(version string) {
+	rootCmd.Version = version
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 /*
 Copyright © 2024 shynome <shynome@gmail.com>
-
 */
 package main
 
 import "github.com/vscode-lcode/hub/cmd"
 
+var Version = "dev"
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(Version)
 }
